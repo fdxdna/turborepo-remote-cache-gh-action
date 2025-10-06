c843548161582aa8257fbf7814d99ad38c4744be
Chris Mclean <83690064+fdx-chris-mclean@users.noreply.github.com>
2025-10-06 16:14:45 +1100

    Update start.ts

--- src/start.ts
+++ src/start.ts
@@ -52,7 +52,7 @@ async function main() {
     saveState("pid", subprocess.pid?.toString());
   } catch (e) {
     info(`Error occurred ${e}`);
-    throw new Error(`Turbo server failed to start on port: ${port}`);
+    throw new Error(`Turbo server failed to start on port: ${port}`, e);
   }
 }
 
